2fc4647299ceeb4f7f3469665d44944587332577
jsvanilla <36719905+jsvanilla@users.noreply.github.com>
2023-04-09 15:49:48 -0600

    ADD create_microservice script

--- api/services/educative_data/main.py
+++ api/services/educative_data/main.py
@@ -8,3 +8,8 @@ app.version = "0.0.0"
 @app.get('/')
 def establish_connection():
     return "educative_data connected!"
+
+
+@app.post('/create')
+def create_educative_data():
+    return "educative_data created!"
